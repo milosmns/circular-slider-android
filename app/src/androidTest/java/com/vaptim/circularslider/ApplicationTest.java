532ead001bd588c962fb4f28a97f67626f68ecab
Milos Marinkovic <milosmns@gmail.com>
2015-03-27 00:34:51 +0100

    Adding project stub [gradle]

---
+++ app/src/androidTest/java/com/vaptim/circularslider/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.vaptim.circularslider;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+        // TODO: some tests should go here..
+    }
+
+}
