0e09cba9bc8f7fd6c0ba4151d93beea4e339e108
Milos Marinkovic <milosmns@gmail.com>
2015-03-28 17:46:39 +0100

    - Adding slider file (needs revision) - Adding demo layout with usage - Adding attributes file for custom attributes

---
+++ app/src/main/java/com/vaptim/circularslider/CircularSlider.java
@@ -0,0 +1,266 @@
+package com.vaptim.circularslider;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CircularSlider extends View {
+
+    /**
+     * Listener interface used to detect when slider moves around.
+     */
+    public static interface OnSliderMovedListener {
+
+        /**
+         * This method is invoked when slider moves, providing position of the slider thumb.
+         *
+         * @param pos Value between 0 and 1 representing the current angle.<br>
+         *            {@code pos = (Angle - StartingAngle) / (2 * Pi)}
+         */
+        public void onSliderMoved(double pos);
+    }
+
+    private int mThumbX;
+    private int mThumbY;
+
+    private int mCircleCenterX;
+    private int mCircleCenterY;
+    private int mCircleRadius;
+
+    private Drawable mThumbImage;
+    private int mPadding;
+    private int mThumbSize;
+    private int mThumbColor;
+    private int mBorderColor;
+    private int mBorderThickness;
+    private double mStartAngle;
+    private double mAngle = mStartAngle;
+    private boolean mIsThumbSelected = false;
+
+    private Paint mPaint = new Paint();
+    private OnSliderMovedListener mListener;
+
+    public CircularSlider(Context context) {
+        this(context, null);
+    }
+
+    public CircularSlider(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircularSlider(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CircularSlider(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr);
+    }
+
+    // common initializer method
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircularSlider, defStyleAttr, 0);
+
+        // read all available attributes
+        float startAngle = a.getFloat(R.styleable.CircularSlider_start_angle, (float) Math.PI / 2);
+        float angle = a.getFloat(R.styleable.CircularSlider_angle, (float) Math.PI / 2);
+        int thumbSize = a.getDimensionPixelSize(R.styleable.CircularSlider_thumb_size, 50);
+        int thumbColor = a.getColor(R.styleable.CircularSlider_thumb_color, Color.GRAY);
+        int borderThickness = a.getDimensionPixelSize(R.styleable.CircularSlider_border_thickness, 20);
+        int borderColor = a.getColor(R.styleable.CircularSlider_border_color, Color.RED);
+        Drawable thumbImage = a.getDrawable(R.styleable.CircularSlider_thumb_image);
+
+        // save those to fields (really, do we need setters here..?)
+        setStartAngle(startAngle);
+        setAngle(angle);
+        setBorderThickness(borderThickness);
+        setBorderColor(borderColor);
+        setThumbSize(thumbSize);
+        setThumbImage(thumbImage);
+        setThumbColor(thumbColor);
+
+        // assign padding - check for version because of RTL layout compatibility
+        int padding;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            padding = (getPaddingLeft() + getPaddingRight() + getPaddingBottom() + getPaddingTop() + getPaddingEnd() + getPaddingStart()) / 6;
+        } else {
+            padding = (getPaddingLeft() + getPaddingRight() + getPaddingBottom() + getPaddingTop()) / 4;
+        }
+        setPadding(padding);
+
+        a.recycle();
+    }
+
+    /* ***** Setters ***** */
+    public void setStartAngle(double startAngle) {
+        mStartAngle = startAngle;
+    }
+
+    public void setAngle(double angle) {
+        mAngle = angle;
+    }
+
+    public void setThumbSize(int thumbSize) {
+        mThumbSize = thumbSize;
+    }
+
+    public void setBorderThickness(int circleBorderThickness) {
+        mBorderThickness = circleBorderThickness;
+    }
+
+    public void setBorderColor(int color) {
+        mBorderColor = color;
+    }
+
+    public void setThumbImage(Drawable drawable) {
+        mThumbImage = drawable;
+    }
+
+    public void setThumbColor(int color) {
+        mThumbColor = color;
+    }
+
+    public void setPadding(int padding) {
+        mPadding = padding;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        // use smaller dimension for calculations (depends on parent size)
+        int smallerDim = w > h ? h : w;
+
+        // find circle's rectangle points
+        int largestCenteredSquareLeft = (w - smallerDim) / 2;
+        int largestCenteredSquareTop = (h - smallerDim) / 2;
+        int largestCenteredSquareRight = largestCenteredSquareLeft + smallerDim;
+        int largestCenteredSquareBottom = largestCenteredSquareTop + smallerDim;
+
+        // save circle coordinates and radius in fields
+        mCircleCenterX = largestCenteredSquareRight / 2 + (w - largestCenteredSquareRight) / 2;
+        mCircleCenterY = largestCenteredSquareBottom / 2 + (h - largestCenteredSquareBottom) / 2;
+        mCircleRadius = smallerDim / 2 - mBorderThickness / 2 - mPadding;
+
+        // works well for now, should we call something else here?
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // outer circle (ring)
+        mPaint.setColor(mBorderColor);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(mBorderThickness);
+        mPaint.setAntiAlias(true);
+        canvas.drawCircle(mCircleCenterX, mCircleCenterY, mCircleRadius, mPaint);
+
+        // find thumb position
+        mThumbX = (int) (mCircleCenterX + mCircleRadius * Math.cos(mAngle));
+        mThumbY = (int) (mCircleCenterY - mCircleRadius * Math.sin(mAngle));
+
+        if (mThumbImage != null) {
+            // draw png
+            mThumbImage.setBounds(mThumbX - mThumbSize / 2, mThumbY - mThumbSize / 2, mThumbX + mThumbSize / 2, mThumbY + mThumbSize / 2);
+            mThumbImage.draw(canvas);
+        } else {
+            // draw colored circle
+            mPaint.setColor(mThumbColor);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawCircle(mThumbX, mThumbY, mThumbSize, mPaint);
+        }
+    }
+
+    /**
+     * Invoked when slider starts moving or is currently moving.
+     * This method calculates and sets position and angle of the thumb.
+     *
+     * @param touchX Where is the touch identifier now on X axis
+     * @param touchY Where is the touch identifier now on Y axis
+     */
+    private void updateSliderState(int touchX, int touchY) {
+        int distanceX = touchX - mCircleCenterX;
+        int distanceY = mCircleCenterY - touchY;
+        //noinspection SuspiciousNameCombination
+        double c = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+        mAngle = Math.acos(distanceX / c);
+        if (distanceY < 0) {
+            mAngle = -mAngle;
+        }
+
+        if (mListener != null) {
+            // notify slider moved listener of the new position which should be in [0..1] range
+            mListener.onSliderMoved((mAngle - mStartAngle) / (2 * Math.PI));
+        }
+    }
+
+    /**
+     * Position setter. This method should be used to manually position the slider thumb.<br>
+     * Note that counterclockwise {@link #mStartAngle} is used to determine the initial thumb position.
+     *
+     * @param pos Value between 0 and 1 used to calculate the angle. {@code Angle = StartingAngle + pos * 2 * Pi}<br>
+     *            Note that angle will not be updated if the position parameter is not in the valid range [0..1]
+     */
+    public void setPosition(double pos) {
+        if (pos >= 0 && pos <= 1) {
+            mAngle = mStartAngle + pos * 2 * Math.PI;
+        }
+    }
+
+    /**
+     * Saves a new slider moved listner. Set {@link com.vaptim.circularslider.CircularSlider.OnSliderMovedListener} to {@code null} to remove it.
+     *
+     * @param listener Instance of the slider moved listener, or null when removing it
+     */
+    public void setOnSliderMovedListener(OnSliderMovedListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    @SuppressWarnings("NullableProblems")
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                // start moving the thumb (this is the first touch)
+                int x = (int) ev.getX();
+                int y = (int) ev.getY();
+                if (x < mThumbX + mThumbSize && x > mThumbX - mThumbSize && y < mThumbY + mThumbSize && y > mThumbY - mThumbSize) {
+                    mIsThumbSelected = true;
+                    updateSliderState(x, y);
+                }
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                // still moving the thumb (this is not the first touch)
+                if (mIsThumbSelected) {
+                    int x = (int) ev.getX();
+                    int y = (int) ev.getY();
+                    updateSliderState(x, y);
+                }
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                // finished moving (this is the last touch)
+                mIsThumbSelected = false;
+                break;
+            }
+        }
+
+        // redraw the whole component
+        invalidate();
+        return true;
+    }
+
+}
